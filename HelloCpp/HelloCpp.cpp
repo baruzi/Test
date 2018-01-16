f3a25cb278f7ba4afd64de3094d594e9a799ed28
baruzi <baruzi33@hotmail.com>
2018-01-16 12:08:00 +0900

    add file
    hahahaa

---
+++ HelloCpp/HelloCpp.cpp
@@ -0,0 +1,14 @@
+// HelloCpp.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	cout << "Hello!";
+	cout << "hello!";
+    return 0;
+}
+
